0f269c5c3b9b864843580cda00c11dc87fc16f7c
Mari2213 <mariannasilvapereira15@gmail.com>
2024-05-17 15:59:39 -0300

    projetos JS

---
+++ src/JavaScript/Projetos/Projeto 3/index.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "violet", "blue", "grey"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+};
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
