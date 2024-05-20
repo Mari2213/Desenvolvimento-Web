4fdea1311efef4e9c31f1dd26943acd7bf2be7e2
Mari2213 <mariannasilvapereira15@gmail.com>
2024-05-20 16:54:20 -0300

    exercicios JS

---
+++ src/JavaScript com NodeJs/Exercícios/Ex 03/index.js
@@ -0,0 +1,8 @@
+console.log(2 < 4);
+console.log(2.3 > 2);
+console.log(22 >= 3);
+console.log(3 <= 21);
+console.log("2" === "3");
+console.log("3" !== "3");
+console.log(3 === 3);
+console.log(3 !== 3);
---
+++ src/JavaScript com NodeJs/Exercícios/Ex 04/index.js
@@ -0,0 +1,3 @@
+console.log(2 < 4 && 3 > 2);
+console.log(2.3 > 2 || 2.3 < 2);
+console.log(22 >= 3 !== 3 <= 22);
