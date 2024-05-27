110faed8c807a6c1d1a7a7aa321d787946d5a003
Mari2213 <mariannasilvapereira15@gmail.com>
2024-05-27 16:18:10 -0300

    exercicios JS

--- src/JavaScript com NodeJs/Exercícios/Ex 40/script.js
+++ src/JavaScript com NodeJs/Exercícios/Ex 40/script.js
@@ -145,36 +145,159 @@ contaMarianna.visualizarSaldos();
 //   }
 // }
 
-class Livro {
-  constructor(titulo, autor) {
-    this.titulo = titulo;
-    this.autor = autor;
-    this.disponivel = true;
-  }
+// class Livro {
+//   constructor(titulo, autor) {
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponivel = true;
+//   }
+//
+//   emprestar() {
+//     if (this.disponivel) {
+//       this.disponivel = false;
+//       console.log("O livro foi emprestado.");
+//     } else {
+//       console.log("Error: O livro não está disponível.");
+//     }
+//   }
+//
+//   devolver() {
+//     if (!this.disponivel) {
+//       this.disponivel = true;
+//       console.log("O livro foi devolvido");
+//     } else {
+//       console.log("Error: O livro já está disponível.");
+//     }
+//   }
+//
+//   consultarDisponibilidade() {
+//     if (this.disponivel) {
+//       console.log("O livro está disponível");
+//     } else {
+//       console.log("O livro não está disponível");
+//     }
+//   }
+// }
 
-  emprestar() {
-    if (this.disponivel) {
-      this.disponivel = false;
-      console.log("O livro foi emprestado.");
-    } else {
-      console.log("Error: O livro não está disponível.");
-    }
-  }
+// let fruits = ["apple", "banana", "orange", "grape"];
+// let selectedFruits;
+//
+// switch (fruits.length) {
+//   case 0:
+//     selectedFruits = "No fruits selected";
+//     break;
+//   case 1:
+//     selectedFruits = fruits[0];
+//     break;
+//   case 2:
+//     selectedFruits = fruits[1];
+//     break;
+//   default:
+//     selectedFruits = fruits[fruits.length - 1];
+// }
+//
+// console.log(selectedFruits);
+//
+// let sales = [120, 150, 200, 180, 90, 160, 210];
+// let total = 0;
+//
+// for (let i = 0; i < sales.length; i++) {
+//   total += sales[i];
+// }
+//
+// let average = total / sales.length;
+//
+// console.log(average);
+//
+// let phrase = "eXamPle PhrAse";
+// let formattedPhrase = phrase.toLocaleLowerCase().split(" ").join("_");
+//
+// console.log(formattedPhrase);
 
-  devolver() {
-    if (!this.disponivel) {
-      this.disponivel = true;
-      console.log("O livro foi devolvido");
-    } else {
-      console.log("Error: O livro já está disponível.");
-    }
-  }
+// function checkPassed(grade) {
+//   const passed = grade.filter((grade) => grade >= 6).length === grade.length;
+//   return passed ? "All students passed" : "Not all students passed";
+// }
+//
+// console.log(checkPassed([7, 8, 9, 6]));
 
-  consultarDisponibilidade() {
-    if (this.disponivel) {
-      console.log("O livro está disponível");
-    } else {
-      console.log("O livro não está disponível");
-    }
-  }
-}
+// function checkPassed(grade) {
+//   const passed = grade.reduce((acc, grade) => acc && grade >= 6, true);
+//   return passed ? "All students passed" : "Not all students passed";
+// }
+//
+// console.log(checkPassed([7, 8, 9, 6]));
+
+// let cart = [
+//   { id: 1, name: "a" },
+//   { id: 2, name: "b" },
+//   { id: 3, name: "c" },
+// ];
+//
+// let productIdToRemove = 2;
+// let updatedCart = cart.filter((product) => product.id !== productIdToRemove);
+//
+// console.log(updatedCart);
+//
+// function Animal() {}
+// function Cachorro() {}
+// Cachorro.prototype = new Animal();
+
+// function isPalindrome(word) {
+//   let reversedWord = word.split("").reverse().join("");
+//   return word === reversedWord;
+// }
+//
+// console.log(isPalindrome("arara"));
+
+// const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+//
+// console.log("foo.bar@gmail.com =======>", emailRegex.test("foo.bar@gmail.com"));
+// console.log(
+//   "foo.bar@gmail.com.br ====>",
+//   emailRegex.test("foo.bar@gmail.com.br"),
+// );
+// console.log(
+//   "foo.bar@gmail.com.br.br =>",
+//   emailRegex.test("foo.bar@gmail.com.br.br"),
+// );
+// console.log("foo.bar@gmail. ==========>", emailRegex.test("foo.bar@gmail."));
+// console.log("foo.bar@gmailcom ========>", emailRegex.test("foo.bar@gmailcom"));
+// console.log("foo.bargmail.com ========>", emailRegex.test("foo.bargmail.com"));
+// console.log("@gmail.com ==============>", emailRegex.test("@gmail.com"));
+
+// function repeatedStrings(arr) {
+//   let counts = {};
+//   let reapeated = [];
+//   arr.forEach((str) => {
+//     counts[str] = (counts[str] || 0) + 1;
+//     if (counts[str] > 1) {
+//       reapeated.push(str);
+//     }
+//   });
+//   return reapeated;
+// }
+//
+// console.log(
+//   repeatedStrings([
+//     "a",
+//     "b",
+//     "a",
+//     "c",
+//     "b",
+//     "a",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//   ]),
+// );
