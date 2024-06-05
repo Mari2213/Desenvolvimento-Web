f13ac2b3f95d5e5cb40cc0c294b38adfb3b72d71
Mari2213 <mariannasilvapereira15@gmail.com>
2024-06-05 17:01:23 -0300

    Modification config db

--- src/JavaScript com NodeJs/Projeto/app.js
+++ src/JavaScript com NodeJs/Projeto/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const db = require("./db/connection");
 
 const PORT = 3000;
 
 app.listen(PORT, function () {
   console.log(`O nosso app esta rodando na porta ${PORT}`);
 });
+
+db.authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso");
+  })
+  .catch((error) => {
+    console.log("Erro ao conectar ao banco de dados: ", error.message);
+    console.log("Erro detalhado: ", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Está funcionando 2");
